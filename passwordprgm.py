8530752e1cba48fa004361b217fe8b4442ee7e83
ken _card_ <krc92001@gmail.com>
2021-11-09 21:05:46 -0500

    added GUI to passwd prog and other small changes

--- passwordprgm.py
+++ passwordprgm.py
@@ -3,13 +3,13 @@ Kenneth Card
 
 Password Program
 
-10/1/21
+10/1/21 - 
 
 This program abides by the password policy created by Kenneth Card - "Password policy.docx"
 
 """
 from os import error
-import tkinter
+from tkinter import *
 import re
 from getpass import getpass
 
@@ -43,12 +43,33 @@ def create_user(username, Current_users):
     new_user = User(username)
     Current_users.append(new_user.username)
     return new_user
+def test_login(password) :
+    if password != None :
+        print("This worked")
+        return True
 
+def GUI():
+    "Creates the GUI and contains the elements of a GUI"
+    #window
+    tkWindow = Tk()  
+    tkWindow.geometry('400x150')  
+    tkWindow.title('Tkinter Login Form - pythonexamples.org')
 
+    #username label and text entry box
+    """usernameLabel = Label(tkWindow, text="User Name").grid(row=0, column=0)
+    username = StringVar
+    usernameEntry = Entry(tkWindow, textvariable=username).grid(row=0, column=1)  """
+
+    #password label and password entry box
+    passwordLabel = Label(tkWindow,text="Password").grid(row=1, column=0)  
+    password = StringVar()
+    passwordEntry = Entry(tkWindow, textvariable=password, show='*').grid(row=1, column=1)
+    loginButton = Button(tkWindow, text="Login", command=lambda :test_login(password)).grid(row=4, column=0)
+    return tkWindow 
 
 
 def main():
-
-
-
+    gui = GUI()
+    gui.mainloop()
+    return 0
 main()
